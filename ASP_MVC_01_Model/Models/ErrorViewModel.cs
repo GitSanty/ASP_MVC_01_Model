512c949c92947348723454df24d09e973588c2ad
Santiago <sastetec@hotmail.com>
2022-05-30 09:03:03 +0200

    Ajoutez des fichiers projet.

---
+++ ASP_MVC_01_Model/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP_MVC_01_Model.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
